3c57967a61ccd2eb763bce8a0a6c72089fc10f24
Chris Rogers <chris@reactrp.com>
2020-11-18 16:14:46 +0000

    6

---
+++ resources/[stream]/[MLOS]/cinema/fxmanifest.lua
@@ -0,0 +1,14 @@
+description 'Vinewood Cinema'
+name 'cinema'
+author ''
+
+files {
+    "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+this_is_a_map 'yes'
+
+fx_version 'bodacious'
+games {'gta5'}
---
+++ resources/[stream]/[MLOS]/laurence_bank/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+
+games {'gta5'}
+
+author 'DragonDoctor (Laurence)'
+description 'DRP Paleto Bank'
+version '1.0.0'
+
+this_is_a_map 'yes'
